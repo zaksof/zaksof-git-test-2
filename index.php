64f6f5ce574676b0fd109f559e112b8d89946923
zaksof <zakysof.68@gmail.com>
2020-07-06 08:05:47 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa eveniet maiores doloremque, sunt corrupti quas nemo, assumenda voluptatum, ducimus hic necessitatibus? Optio delectus tenetur praesentium amet et ipsum explicabo voluptas.</p>
+    <ul>
+        <li>Satu</li>
+        <li>Dua</li>
+    </ul>
 </body>
 </html>
