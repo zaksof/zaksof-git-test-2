a2f0da6dd9f930d8bf7472462c2367d8c32d12d3
zaksof <zakysof.68@gmail.com>
2020-07-06 08:00:46 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa eveniet maiores doloremque, sunt corrupti quas nemo, assumenda voluptatum, ducimus hic necessitatibus? Optio delectus tenetur praesentium amet et ipsum explicabo voluptas.</p>
 </body>
 </html>
